7bb08bb52b60da33f09648cd391d47d71c34d1e9
georgeb79 <georgebaez123@hotmail.com>
2022-12-03 18:25:51 -0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./index.scss";
+
+function App() {
+  const [count, setCount] = useState([]);
+
+  return (
+    <div className='App'>
+
+    </div>
+  );
+}
+
+export default App;
