fa1c179dbaa8df32f42f64719b8c7e9cce836583
GeorgeB <g.baez@gbh.com.do>
2022-12-23 20:57:59 -0400

    Add Icon funtionality to Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { ButtonProps } from "@/interfaces";
 import "./index.scss";
 
-export function Button({ text, color, type, onClick }: ButtonProps) {
+export function Button({ text, color, type, onClick, icon }: ButtonProps) {
   return (
     <>
       <button onClick={onClick} className={`btn btn--${type}`}>
-        {text}
+        {text && text.length > 0 ? text : icon}
       </button>
     </>
   );
