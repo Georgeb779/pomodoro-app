80339eedace4690b20bbb93514f07eb0513ba121
GeorgeB <g.baez@gbh.com.do>
2022-12-23 20:57:21 -0400

    Add icons (Close and Gear)

---
+++ src/assets/icons/Close.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function Close() {
+    return (
+        <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g opacity="0.5">
+                <path fillRule="evenodd" clipRule="evenodd" d="M13.3639 2.05044L11.9497 0.63623L6.99995 5.58598L2.0502 0.63623L0.635986 2.05044L5.58573 7.00019L0.635986 11.9499L2.0502 13.3642L6.99995 8.4144L11.9497 13.3642L13.3639 11.9499L8.41416 7.00019L13.3639 2.05044Z" fill="#1E213F" />
+            </g>
+        </svg>
+
+    );
+}
