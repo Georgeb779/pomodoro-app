975be7d8b4951e949359e761fcba9d8ef0564172
Georgeb779 <georgebaez123@hotmail.com>
2022-12-25 16:15:58 -0400

    Creaate PomodoroTimeInputProps component

---
+++ src/components/PomodoroTimeInput/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+type PomodoroTimeInputProps = {
+  label: string;
+  name: string;
+  id: string;
+  value: number;
+  setValue: (value: number) => void;
+};
+
+export function PomodoroTimeInput({
+  label,
+  name,
+  id,
+  value,
+  setValue
+}: PomodoroTimeInputProps) {
+  return (
+    <span>
+      <label htmlFor={id}>{label}</label>{" "}
+      <input
+        type='number'
+        name={name}
+        id={id}
+        value={value}
+        onChange={(e) => {
+          setValue(Number(e.target.value));
+        }}
+        min={1}
+        max={60}
+        step={1}
+      />
+    </span>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export * from './Modal';
 export * from './CountDownBar';
 export * from './ColorItem';
 export * from './FontItem';
+export * from './PomodoroTimeInput';
